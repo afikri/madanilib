6e1c9f66b94fdce06886da4e989d63ec843b64c3
afikri <fikri.abdul@gmx.com>
2022-07-03 17:01:06 +0700

    Fixed login page

---
+++ api/database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                "name" => "Fikri Abdul",
+                "email" => "fikri.abdul@gmx.com",
+                "password" => bcrypt("p455w0rd@123"),
+                "remember_token" =>Str::random(10), 
+                "created_at" => now(),
+                "updated_at" => now()
+            ],
+
+        ]);
+    }
+}
--- ui/src/pages/authentication/Login.js
+++ ui/src/pages/authentication/Login.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import palm from '../../images/palm-oil.jpg'
 import './Login.css';
-import palm from '../../images/palm-oil.jpg';
 
 const Login = () => {
 
@@ -45,9 +45,102 @@ const Login = () => {
   }
 
   return (
-    <div className='grid-container'>
-      <div className='grid-item left' style={{ border: "2px solid black" }}><h1>This is left</h1></div>
-      <div className='grid-item right' style={{ border: "2px solid black" }}><h1>This is right</h1></div>
+    <div className="limiter">
+      <div className="container-login100">
+        <div className="wrap-login100">
+          {
+            validation.message && (
+              <div>
+                {validation.message}
+              </div>
+            )
+          }
+          <form className="login100-form validate-form" onSubmit={handleLogin}>
+            <span className="login100-form-title p-b-43">
+              MadaniLib
+            </span>
+
+            <div className="wrap-input100 validate-input">
+              <input className="input100"
+                type="text"
+                value={email}
+                onChange={(e) => { setEmail(e.target.value) }}
+              />
+              <span className="focus-input100"></span>
+              <span className="label-input100">Email</span>
+            </div>
+            {
+              validation.email && (
+                <div>
+                  {validation.email[0]}
+                </div>
+              )
+            }
+
+            <div className="wrap-input100 validate-input">
+              <input className="input100"
+                type="password"
+                value={password}
+                onChange={(e) => { setPassword(e.target.value) }}
+              />
+              <span className="focus-input100"></span>
+              <span className="label-input100">Password</span>
+            </div>
+            {
+              validation.password && (
+                <div>
+                  {validation.password[0]}
+                </div>
+              )
+            }
+
+            <div className="flex-sb-m w-full p-t-3 p-b-32">
+              <div className="contact100-form-checkbox">
+                <input className="input-checkbox100" id="ckb1" type="checkbox" name="remember-me" />
+                {/* <label htmlFor="label-checkbox100" for="ckb3">
+                Ingat saya
+                </label> */}
+              </div>
+
+              <div>
+                <Link to="#" className="txt1" style={{ textDecoration: "none" }}>
+                  Lupa Password?
+                </Link>
+              </div>
+            </div>
+
+
+            <div className="container-login100-form-btn">
+              <button className="login100-form-btn" >
+                Login
+              </button>
+            </div>
+
+            <div className="text-center p-t-46 p-b-20">
+              <span className="txt2">
+                atau Sign Up dengan
+              </span>
+            </div>
+
+            <div className="login100-form-social flex-c-m">
+              <Link to="#" className="login100-form-social-item flex-c-m bg3 m-r-5" style={{ textDecoration: "none" }}>
+                <i className="fa fa-google" aria-hidden="true"></i>
+              </Link>
+              <Link to="#" className="login100-form-social-item flex-c-m bg1 m-r-5" style={{ textDecoration: "none" }}>
+                <i className="fa fa-facebook-f" aria-hidden="true"></i>
+              </Link>
+
+              <Link to="#" className="login100-form-social-item flex-c-m bg2 m-r-5" style={{ textDecoration: "none" }}>
+                <i className="fa fa-twitter" aria-hidden="true"></i>
+              </Link>
+            </div>
+          </form>
+
+          <div className="login100-more"  >
+            <img src={palm} width="100%" height="100%" alt="palm-oil" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
